ca058ff30f087e1d60a87e7d38b7eb8751351f16
kamareg <kamareggg@gmail.com>
2022-05-19 10:59:08 +0300

    Initial commit

---
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/Main.java
@@ -0,0 +1,29 @@
+package ua.edu.sumdu.j2se.malikova.tasks;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Task task1 = new Task("first task", 12);
+        Task task2 = new Task("second task", 5,100,20);
+        task2.setActive(true);
+        int a = task2.nextTimeAfter(15);
+        System.out.println(task1);
+        System.out.println(task2);
+        String b = task1.getTitle();
+        System.out.println(b);
+        task1.setTitle("one");
+        System.out.println(task1);
+        System.out.println(a);
+        int c = task1.getTime();
+        int d = task2.getTime();
+        System.out.println(c);
+        System.out.println(d);
+        task2.setTime(8);
+        System.out.println(task2);
+        int e = task2.nextTimeAfter(6);
+        System.out.println(e);
+    }
+
+
+}
---
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/Task.java
@@ -0,0 +1,263 @@
+package ua.edu.sumdu.j2se.malikova.tasks;
+
+/**
+ * Клас, що містить конструктори та методи для роботи з задачами.
+ */
+public class Task {
+
+    private String title;
+    private int time;
+    private int start;
+    private int end;
+    private int interval;
+    private boolean active;
+    private boolean isRepeated;
+
+
+    /**
+     * Конструктор, що конструює неактивну задачу,
+     * яка виконується у заданий час без повторення.
+     *
+     * @param title назва задачі.
+     * @param time  час виконання задачі.
+     */
+
+    public Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+        setActive(false);
+        setRepeated(false);
+    }
+
+    /**
+     * Конструктор, що конструює неактивну задачу,
+     * яка виконується у заданому проміжку часу із заданим інтервалом.
+     *
+     * @param title    назва задачі.
+     * @param start    час початку виконання задачі.
+     * @param end      час закінчення виконання задачі.
+     * @param interval інтервал часу виконання задачі.
+     */
+
+    public Task(String title, int start, int end, int interval) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+        setActive(false);
+        setRepeated(true);
+    }
+
+    /**
+     * Метод для повернення назви задачі.
+     *
+     * @return назва задачі.
+     */
+
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Метод для встановлення назви задачі.
+     *
+     * @param title назва задачі.
+     */
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Метод для повернення стану задачі.
+     *
+     * @return стан задачі.
+     */
+
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * Метод для встановлення стану задачі.
+     *
+     * @param active стан задачі.
+     */
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * Метод для повернення часу виконання задачі, що не повторюється.
+     *
+     * @return у разі, якщо задача повторюється метод має
+     * повертати час початку повторення.
+     */
+
+    public int getTime() {
+        if (isRepeated()) {
+            return start;
+        } else {
+            return time;
+        }
+    }
+
+    /**
+     * Метод для зміни часу виконання задачі, що не повторюється.
+     * У разі, якщо задача повторювалась, вона має стати такою,
+     * що не повторюється.
+     *
+     * @param time час виконання задачі.
+     */
+
+    public void setTime(int time) {
+        if (isRepeated()) {
+            setRepeated(false);
+        }
+        this.time = time;
+    }
+
+    /**
+     * Метод для повернення часу виконання задачі, що повторюється.
+     *
+     * @return у разі, якщо задача не повторюється метод
+     * має повертати час виконання задачі
+     */
+
+    public int getStartTime() {
+        if (!isRepeated()) {
+            return time;
+        } else {
+            return start;
+        }
+    }
+
+    /**
+     * Метод для повернення часу виконання задачі, що повторюється.
+     *
+     * @return у разі, якщо задача не повторюється метод має
+     * повертати час виконання задачі.
+     */
+
+    public int getEndTime() {
+        if (!isRepeated()) {
+            return time;
+        } else {
+            return end;
+        }
+    }
+
+    /**
+     * Метод для повернення часу виконання задачі, що повторюється.
+     *
+     * @return у разі, якщо задача не повторюється метод має
+     * повертати 0.
+     */
+
+    public int getRepeatInterval() {
+        if (!isRepeated()) {
+            return 0;
+        } else {
+            return interval;
+        }
+    }
+
+    /**
+     * Метод для зміни часу виконання задачі, що повторюється. У разі,
+     * якщо задача не повторювалася вона має стати такою,
+     * що повторюється.
+     *
+     * @param start    час початку виконання задачі.
+     * @param end      час закінчення виконання задачі.
+     * @param interval інтервал часу виконання задачі.
+     */
+
+    public void setTime(int start, int end, int interval) {
+        if (!isRepeated()) {
+            setRepeated(true);
+        }
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+    }
+
+    /**
+     * Метод для перевірки повторюваності задачі.
+     *
+     * @return результат перевірки повторюваності задачі.
+     */
+
+    public boolean isRepeated() {
+        return isRepeated;
+    }
+
+    /**
+     * Метод для встановлення повторюваності задачі.
+     *
+     * @param repeated зміна стану повторюванності задачі.
+     */
+
+    public void setRepeated(boolean repeated) {
+        this.isRepeated = repeated;
+    }
+
+    /**
+     * Метод повертає час наступного виконання задачі після вказаного часу.
+     *
+     * @param current вказанний час.
+     * @return якщо після вказаного часу задача не виконується, то
+     * метод має повертати -1.
+     */
+
+    public int nextTimeAfter(int current) {
+        if (!this.isActive()) {
+            return -1;
+        } else if (!this.isRepeated()) {
+            if (current < this.getStartTime()) {
+                return this.getStartTime();
+            } else {
+                return -1;
+            }
+        } else {
+            if (current < this.getStartTime()) {
+                return this.getStartTime();
+            } else {
+                int i = this.getStartTime();
+                while (i < this.getEndTime()) {
+                    if (i <= current) {
+                        i = i + this.getRepeatInterval();
+                    } else {
+                        break;
+                    }
+                }
+                if (this.getEndTime() > i) {
+                    return i;
+                } else {
+                    return -1;
+                }
+            }
+        }
+    }
+
+    /**
+     * Метод перевизначення.
+     *
+     * @return повертає дані в зручному вигляді.
+     */
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", time=" + time +
+                ", start=" + start +
+                ", end=" + end +
+                ", interval=" + interval +
+                ", active=" + active +
+                ", isRepeated=" + isRepeated +
+                '}';
+    }
+}
+
+

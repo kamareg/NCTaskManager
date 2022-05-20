ec9c800d6f54777fe9b923d8191d2499697f4e9d
kamareg <kamareggg@gmail.com>
2022-05-20 14:45:12 +0300

    array task list

---
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/ArrayTaskList.java
@@ -0,0 +1,28 @@
+﻿package ua.edu.sumdu.j2se.malikova.tasks;
+
+public class ArrayTaskList {
+
+    public void add(Task task) {
+
+    }
+    public boolean remove (Task task) {
+
+        return false;
+    }
+    public int size() {
+
+        return 0;
+    }
+
+    public Task getTask (int index) {
+
+        return null;
+    }
+
+    public ArrayTaskList incoming (int from, int to)
+    {
+
+        return null;
+    }
+}
+

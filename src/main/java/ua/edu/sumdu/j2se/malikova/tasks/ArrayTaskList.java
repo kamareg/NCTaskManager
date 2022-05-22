959dd2cf1c7739ddcda98a4b3fdd5bb3b863ee40
kamareg <kamareggg@gmail.com>
2022-05-23 01:40:10 +0300

    array task list

--- src/main/java/ua/edu/sumdu/j2se/malikova/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/ArrayTaskList.java
@@ -1,28 +1,63 @@
-﻿package ua.edu.sumdu.j2se.malikova.tasks;
+package ua.edu.sumdu.j2se.malikova.tasks;
+
+import java.util.Arrays;
 
 public class ArrayTaskList {
+    private Task[] array = new Task[0];
+    private int count;
+
+    public ArrayTaskList (Task[] array, int count) {
+    this.array = array;
+    this.count = count;
+    }
+    public ArrayTaskList () {super();}
 
     public void add(Task task) {
-
+        if (array.length == count) {
+            array = Arrays.copyOf(array, (array.length + 3));
+        }
+        array[count] = task;
+        count++;
     }
-    public boolean remove (Task task) {
 
+    public boolean remove(Task task) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == task) {
+                Task[] arrayForRemove = new Task[array.length];
+                for (int index = 0; index < i; index++) {
+                    arrayForRemove[index] = array[index];
+                }
+                for (int j = i+1; j < array.length - 1; j++) {
+                    arrayForRemove[j] = array[j];
+                }
+                array = Arrays.copyOf(arrayForRemove, (arrayForRemove.length));
+                return true;
+
+            } // break;
+
+        }
         return false;
     }
+
     public int size() {
-
-        return 0;
+        return array.length;
     }
 
-    public Task getTask (int index) {
+    public Task getTask(int index) {
+        return array[index];
+    }
+
+    public ArrayTaskList incoming(int from, int to) {
 
         return null;
     }
 
-    public ArrayTaskList incoming (int from, int to)
-    {
-
-        return null;
+    @Override
+    public String toString() {
+        return "ArrayTaskList{" +
+                "array=" + Arrays.toString(array) +
+                ", count=" + count +
+                '}';
     }
 }
 
--- src/main/java/ua/edu/sumdu/j2se/malikova/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/Main.java
@@ -4,25 +4,46 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        Task task1 = new Task("first task", 12);
-        Task task2 = new Task("second task", 5,100,20);
-        task2.setActive(true);
-        int a = task2.nextTimeAfter(35);
-        System.out.println(task1);
-        System.out.println(task2);
-        String b = task1.getTitle();
-        System.out.println(b);
-        task1.setTitle("one");
-        System.out.println(task1);
-        System.out.println(a);
-        int c = task1.getTime();
-        int d = task2.getTime();
-        System.out.println(c);
-        System.out.println(d);
-        task2.setTime(8);
-        System.out.println(task2);
-        int e = task2.nextTimeAfter(6);
-        System.out.println(e);
+        Task task1 = new Task("first task", 1);
+        Task task2 = new Task("second task", 3, 100, 20);
+        Task task3 = new Task("third task", 5);
+        Task task4 = new Task("fourth task", 10);
+        Task task5 = new Task("fifth task", 15);
+        Task task6 = new Task("sixth task", 20);
+        Task task7 = new Task("seventh task", 9, 90, 25);
+        Task task8 = new Task("eighth task", 17, 50, 4);
+        Task task9 = new Task("ninth task", 28, 48, 2);
+        Task task10 = new Task("tenth task", 40, 69, 5);
+
+
+        ArrayTaskList one = new ArrayTaskList(new Task[4], 0);
+        one.add(task1);
+        one.add(task2);
+        one.add(task3);
+        one.add(task4);
+        one.add(task5);
+
+        System.out.println(one);
+
+
+
+        // task2.setActive(true);
+        // int a = task2.nextTimeAfter(35);
+        // System.out.println(task1);
+        // System.out.println(task2);
+        // String b = task1.getTitle();
+        // System.out.println(b);
+        // task1.setTitle("one");
+        // System.out.println(task1);
+        // System.out.println(a);
+        // int c = task1.getTime();
+        // int d = task2.getTime();
+        // System.out.println(c);
+        // System.out.println(d);
+        // task2.setTime(8);
+        // System.out.println(task2);
+        // int e = task2.nextTimeAfter(6);
+        // System.out.println(e);
     }
 
 

f27903eb3e4633bb16b0835a14800e0158f9cb80
kamareg <kamareggg@gmail.com>
2022-05-26 00:33:17 +0300

    array task list

--- src/main/java/ua/edu/sumdu/j2se/malikova/tasks/ArrayTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/ArrayTaskList.java
@@ -4,43 +4,48 @@ import java.util.Arrays;
 
 public class ArrayTaskList {
     private Task[] array = new Task[0];
-    private int count;
+    private int counter;
 
-    public ArrayTaskList (Task[] array, int count) {
-    this.array = array;
-    this.count = count;
+    public ArrayTaskList() {
+        super();
     }
-    public ArrayTaskList () {super();}
 
     public void add(Task task) {
-        if (array.length == count) {
+        if (array.length == counter) {
             array = Arrays.copyOf(array, (array.length + 3));
         }
-        array[count] = task;
-        count++;
+        array[counter] = task;
+        counter++;
     }
 
     public boolean remove(Task task) {
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] == task) {
                 Task[] arrayForRemove = new Task[array.length];
                 for (int index = 0; index < i; index++) {
                     arrayForRemove[index] = array[index];
                 }
-                for (int j = i+1; j < array.length - 1; j++) {
-                    arrayForRemove[j] = array[j];
+                for (int j = i + 1; j < array.length; j++, i++) {
+                    arrayForRemove[i] = array[j];
                 }
                 array = Arrays.copyOf(arrayForRemove, (arrayForRemove.length));
+                counter--;
                 return true;
-
-            } // break;
-
+            }
         }
         return false;
     }
 
     public int size() {
-        return array.length;
+        int countingTasks = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] != null) {
+                countingTasks += 1;
+            }
+        }
+        return countingTasks;
+
     }
 
     public Task getTask(int index) {
@@ -48,15 +53,25 @@ public class ArrayTaskList {
     }
 
     public ArrayTaskList incoming(int from, int to) {
+        ArrayTaskList incomingArray = new ArrayTaskList();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                if (array[i].nextTimeAfter(from) != (-1)) {
+                    if (array[i].nextTimeAfter(from) < to) {
+                        incomingArray.add(array[i]);
+                    }
+                }
+            }
+        }
+        return incomingArray;
 
-        return null;
+
     }
 
     @Override
     public String toString() {
-        return "ArrayTaskList{" +
-                "array=" + Arrays.toString(array) +
-                ", count=" + count +
+        return "array=" + Arrays.toString(array) +
+                ", count=" + counter +
                 '}';
     }
 }
--- src/main/java/ua/edu/sumdu/j2se/malikova/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/Main.java
@@ -3,7 +3,41 @@ package ua.edu.sumdu.j2se.malikova.tasks;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+       /* Task task1 = new Task("Simple IN", 55, true);
+        Task task2 = new Task("Simple OUT", 10, true);
+        Task task3 = new Task("Inactive OUT", 0, 1000, 1, false);
+        Task task4 = new Task("Simple bound OUT", 50, true);
+        Task task5 = new Task("Simple bound IN", 60, true);
+        Task task6 = new Task("Repeat inside IN", 51, 58, 2, true);
+        Task task7 = new Task("Repeat outside IN", 0, 100, 5, true);
+        Task task8 = new Task("Repeat outside OUT", 0, 100, 22, true);
+        Task task9 = new Task("Repeat left OUT", 0, 40, 1, true);
+        Task task10 = new Task("Repeat right OUT", 65, 100, 1, true);
+        Task task11 = new Task("Repeat left intersect IN 1", 0, 55, 13, true);
+        Task task12 = new Task("Repeat left intersect IN 2", 0, 60, 30, true);
+        Task task13 = new Task("Repeat left intersect OUT", 0, 55, 22, true);
+        Task task14 = new Task("Repeat right intersect IN", 55, 100, 20, true);
+
+ArrayTaskList taskList = new ArrayTaskList();
+taskList.add(task1);
+        taskList.add(task2);
+        taskList.add(task3);
+        taskList.add(task4);
+        taskList.add(task5);
+        taskList.add(task6);
+        taskList.add(task7);
+        taskList.add(task8);
+        taskList.add(task9);
+        taskList.add(task10);
+        taskList.add(task11);
+        taskList.add(task12);
+        taskList.add(task13);
+        taskList.add(task14);
+
+        ArrayTaskList y = taskList.incoming(50,60);
+        System.out.println(y);*/
+
+
         Task task1 = new Task("first task", 1);
         Task task2 = new Task("second task", 3, 100, 20);
         Task task3 = new Task("third task", 5);
@@ -16,15 +50,39 @@ public class Main {
         Task task10 = new Task("tenth task", 40, 69, 5);
 
 
-        ArrayTaskList one = new ArrayTaskList(new Task[4], 0);
+        ArrayTaskList one = new ArrayTaskList();
         one.add(task1);
         one.add(task2);
         one.add(task3);
         one.add(task4);
         one.add(task5);
+        one.add(task1);
+        one.add(task1);
 
+
+        System.out.println(one.size());
+        one.remove(task1);
+        one.remove(task1);
+        one.remove(task2);
+        one.remove(task2);
+        one.remove(task5);
+
+        one.add(task1);
+        one.add(task3);
+        one.add(task2);
+        System.out.println(one.getTask(1));
+        one.add(task4);
+        one.add(task5);
+        one.add(task6);
+        one.add(task7);
+        one.add(task8);
+
+        one.remove(task2);
+        one.remove(task4);
+
+        System.out.println(one.size());
         System.out.println(one);
-
+        one.incoming(2, 5);
 
 
         // task2.setActive(true);
@@ -48,3 +106,4 @@ public class Main {
 
 
 }
+

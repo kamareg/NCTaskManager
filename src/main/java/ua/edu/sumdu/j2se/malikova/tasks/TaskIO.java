f5825a3373ea5a3566a1dc2cf6c03826922f0bd4
kamareg <kamareggg@gmail.com>
2022-06-29 16:39:30 +0300

    process

--- src/main/java/ua/edu/sumdu/j2se/malikova/tasks/TaskIO.java
+++ src/main/java/ua/edu/sumdu/j2se/malikova/tasks/TaskIO.java
@@ -1,86 +1,90 @@
 package ua.edu.sumdu.j2se.malikova.tasks;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonWriter;
+
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
 
 public class TaskIO {
-    public static void write (AbstractTaskList tasks, OutputStream out) {
-        try {
-            DataOutputStream outputStream = new DataOutputStream(out);
+    public static void write(AbstractTaskList tasks, OutputStream out) {
+        try (DataOutputStream outputStream = new DataOutputStream(out)) {
             outputStream.writeInt(tasks.size());
-            for (Task task: tasks) {
-            outputStream.writeInt(task.getTitle().length());
-            outputStream.writeUTF(task.getTitle());
-            outputStream.writeBoolean(task.isActive());
-            outputStream.writeInt(task.getRepeatInterval());
-            if (task.isRepeated()) {
-                outputStream.writeLong(task.getStartTime().toEpochSecond(ZoneOffset.UTC));
-                outputStream.writeLong(task.getEndTime().toEpochSecond(ZoneOffset.UTC));
-            } else {
-                outputStream.writeLong(task.getTime().toEpochSecond(ZoneOffset.UTC));
-            }
+            for (Task task : tasks) {
+                outputStream.writeInt(task.getTitle().length());
+                outputStream.writeUTF(task.getTitle());
+                outputStream.writeBoolean(task.isActive());
+                outputStream.writeInt(task.getRepeatInterval());
+                if (task.isRepeated()) {
+                    outputStream.writeLong(task.getStartTime().toEpochSecond(ZoneOffset.UTC));
+                    outputStream.writeLong(task.getEndTime().toEpochSecond(ZoneOffset.UTC));
+                } else {
+                    outputStream.writeLong(task.getTime().toEpochSecond(ZoneOffset.UTC));
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    void read(AbstractTaskList tasks, InputStream in) {
-        try {
-            DataInputStream inputStream = new DataInputStream(in);
+
+    public static void read(AbstractTaskList tasks, InputStream in) {
+        Task task;
+        try (DataInputStream inputStream = new DataInputStream(in)) {
             int size = inputStream.readInt();
             while (inputStream.available() > 0) {
                 int titleLength = inputStream.readInt();
                 String title = inputStream.readUTF();
-                Boolean isActive = inputStream.readBoolean();
+                boolean isActive = inputStream.readBoolean();
                 int repeatInterval = inputStream.readInt();
-                if (repeatInterval!=0) {
+                if (repeatInterval != 0) {
                     LocalDateTime startTime = LocalDateTime.ofEpochSecond(inputStream.readLong(), 0, ZoneOffset.UTC);
                     LocalDateTime endTime = LocalDateTime.ofEpochSecond(inputStream.readLong(), 0, ZoneOffset.UTC);
+                    task = new Task(title, startTime, endTime, repeatInterval);
                 } else {
                     LocalDateTime time = LocalDateTime.ofEpochSecond(inputStream.readLong(), 0, ZoneOffset.UTC);
+                    task = new Task(title, time);
                 }
+                task.setActive(isActive);
+                tasks.add(task);
             }
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    void writeBinary(AbstractTaskList tasks, File file) {
-
-    }
-
-    void readBinary(AbstractTaskList tasks, File file) {
-
-    }
-
-    void write(AbstractTaskList tasks, Writer out) {
-
-    }
-
-    void read(AbstractTaskList tasks, Reader in) {
-
-    }
-
-    void writeText(AbstractTaskList tasks, File file) {
-
-    }
-
-    void readText(AbstractTaskList tasks, File file) {
-
-    }
-     /*   FileInputStream inputStream = new FileInputStream("c:/data.txt");
-        long sum = 0;
-
-        while (inputStream.available() > 0) //пока остались непрочитанные байты
-        {
-            int data = inputStream.read(); //прочитать очередной байт
-            sum += data; //добавить его к общей сумме
+    public static void writeBinary(AbstractTaskList tasks, File file) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            TaskIO.write(tasks, fileOutputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        inputStream.close(); // закрываем поток
+    }
 
-        System.out.println(sum); //выводим сумму на экран.
-    }*/
+    public static void readBinary(AbstractTaskList tasks, File file) {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            TaskIO.read(tasks, fileInputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void write(AbstractTaskList tasks, Writer out) {
+        try {GsonBuilder gsonBuilder = new GsonBuilder();
+        }
+    }
+
+    public static void read(AbstractTaskList tasks, Reader in) {
+
+    }
+
+    public static void writeText(AbstractTaskList tasks, File file) {
+
+    }
+
+    public static void readText(AbstractTaskList tasks, File file) {
+
+    }
+
 }
